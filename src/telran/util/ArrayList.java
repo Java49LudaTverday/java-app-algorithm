e320098a032a2499fd1934732a1f58f249435173
LudaTverday <Ludachka22@gmail.com>
2023-03-30 13:07:50 +0300

    initial code, implementation methods: add,remove,get

---
+++ src/telran/util/ArrayList.java
@@ -0,0 +1,72 @@
+package telran.util;
+
+import java.util.Arrays;
+
+public class ArrayList<T> implements List<T> {
+	private static final int DEFAULT_CAPASITY = 16;
+	private T[] array;
+	private int size;
+
+	@SuppressWarnings("unchecked")
+	public ArrayList(int capacity) {
+		array = (T[]) new Object[capacity];
+	}
+
+	public ArrayList() {
+		this(DEFAULT_CAPASITY);
+	}
+
+	@Override
+	public boolean add(T obj) {
+		if (size == array.length) {
+			reallocate();
+		}
+		array[size] = obj;
+		size++;
+		return true;
+	}
+
+	private void reallocate() {
+		array = Arrays.copyOf(array, array.length * 2);
+
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public void add(int index, T obj) {
+		if (size == array.length) {
+			reallocate();
+		}
+		if (index > -1 && index <= size) {
+			System.arraycopy(array, index, array, index + 1, size - index);
+			array[index] = obj;
+			size++;
+		}
+
+	}
+
+	@Override
+	public T remove(int index) {
+		T removedObj = null;
+		if (index > -1 && index < size) {
+			removedObj = array[index];
+			System.arraycopy(array, index + 1, array, index, size - index);
+			size--;
+		}
+		return removedObj;
+	}
+
+	@Override
+	public T get(int index) {
+		T object = null;
+		if (index > -1 && index < size) {
+			object = array[index];
+		}
+		return object;
+	}
+
+}
---
+++ src/telran/util/test/ArrayListTest.java
@@ -0,0 +1,101 @@
+package telran.util.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import telran.util.ArrayList;
+
+class ArrayListTest {
+	
+	
+
+	@Test
+	void test() {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		ArrayList<String> strings = new ArrayList<>();
+		numbers.add(1);
+		numbers.add(2);
+
+		strings.add("ABC");
+		assertEquals(2, numbers.size());
+		assertEquals(1, strings.size());
+
+		numbers.add(0, 100);
+		assertEquals(3, numbers.size());
+		numbers.add(numbers.size(), 200);
+		assertEquals(4, numbers.size());
+
+		for (int i = 0; i < 12; i++) {
+			numbers.add(i, i + 1000);
+		}
+		assertEquals(16, numbers.size());
+		numbers.add(numbers.size(), 1);
+		assertEquals(17, numbers.size());
+	}
+	@Test
+	//@Disabled
+	void testAddElemBoundOfIndex () {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			numbers.add(i, i + 1000);
+		}
+		numbers.add(-1, 5);
+		int expected10 = 10;
+		assertEquals(expected10, numbers.size());
+		numbers.add(numbers.size()+1,8);
+		assertEquals(expected10, numbers.size());
+	}
+
+	@Test
+	void testRemoveElem() {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			numbers.add(i, i + 1000);
+		}
+		numbers.remove(9);
+		assertEquals(9, numbers.size());
+		int remuvedNum = numbers.remove(0);
+		assertEquals(8, numbers.size());
+		assertEquals(1000, remuvedNum);
+	}
+	@Test
+	@Disabled
+	void testRemoveElemBoundOfIndex () {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		for (int i = 0; i < 11; i++) {
+			numbers.add(i, i + 1000);
+		}
+		int expectedNull = 0;
+		assertEquals(expectedNull, numbers.remove(-1));
+		assertEquals(expectedNull, numbers.remove(numbers.size()));
+	}
+	
+	@Test
+	void testGetElem() {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		for (int i = 0; i < 11; i++) {
+			numbers.add(i, i + 1000);
+		}
+		int expected1000 = 1000;
+		int expected1010 =1010 ;
+		int expected1005 = 1005 ;
+		assertEquals(expected1000, numbers.get(0));
+		assertEquals(expected1010, numbers.get(numbers.size() - 1));
+		assertEquals(expected1005, numbers.get(5));
+	}
+	@Test
+	@Disabled
+	void testGetElemBoundOfIndex () {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		for (int i = 0; i < 11; i++) {
+			numbers.add(i, i + 1000);
+		}
+		int expected1000 = 1000;
+		assertEquals(expected1000, numbers.get(-1));
+		assertEquals(expected1000, numbers.get(numbers.size()));
+	}
+			
+
+}
