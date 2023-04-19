6d0ca8f0e33422f71ab6dbb92ab1730f92a396a5
LudaTverday <Ludachka22@gmail.com>
2023-04-19 18:45:24 +0300

    adding Math.abs to Integer.compare

--- src/telran/util/test/EvenOddComparator.java
+++ src/telran/util/test/EvenOddComparator.java
@@ -7,16 +7,14 @@ public class EvenOddComparator implements Comparator<Integer> {
 	@Override
 	public int compare(Integer o1, Integer o2) {
 
-		int remanderO1 = o1 < 0 ? (-o1) % 2 : o1 % 2;
-		int remanderO2 = o2 < 0 ? (-o2) % 2 : o2 % 2;
+		//int remanderO1 = o1 < 0 ? (-o1) % 2 : o1 % 2;
+		int remanderO1 = Math.abs(o1 % 2);
+		//int remanderO2 = o2 < 0 ? (-o2) % 2 : o2 % 2;
+		int remanderO2 = Math.abs(o2 % 2);
 
 		int res = Integer.compare(remanderO1, remanderO2);
 		if (res == 0) {
-			if (remanderO1 == 0) {
-				res = o1.compareTo(o2);
-			} else {
-				res = o2.compareTo(o1);
-			}
+			res = (remanderO1 == 0)? o1.compareTo(o2): o2.compareTo(o1);			
 		}
 		return res;
 	}
