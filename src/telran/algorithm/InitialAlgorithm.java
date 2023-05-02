455c51c172eacb4bac7466af0d10afe1ca20c824
LudaTverday <Ludachka22@gmail.com>
2023-05-02 12:11:03 +0300

    updated BinarySearch

--- src/telran/algorithm/InitialAlgorithm.java
+++ src/telran/algorithm/InitialAlgorithm.java
@@ -1,5 +1,7 @@
 package telran.algorithm;
 
+import java.util.Comparator;
+
 public class InitialAlgorithm {
 	public static void sortShortPositive(short[] array) {
 		int[] helper = new int[Short.MAX_VALUE];
@@ -18,27 +20,19 @@ public class InitialAlgorithm {
 	}
 
 	public static boolean isSum2(short[] array, short sum) {
+		int helperSize = sum < 0 ? Short.MAX_VALUE + 1 : sum + 1;
+		boolean[] helper = new boolean[helperSize];
 		boolean res = false;
-		int[] helper = new int[sum + 1];
 		int index = 0;
 		while (index < array.length && !res) {
-			int indexHelper = array[index];
-			if (indexHelper <= sum) {
-				//Solution 1:
-//					helper[indexHelper]++;
-//					if (helper[sum - indexHelper] > 0 && sum - indexHelper != indexHelper) {
-//						res = true;
-//						//if sum even and an array haves two numbers == sum/2
-//					} else if (helper[sum - indexHelper] > 1 && sum - indexHelper == indexHelper) {
-//						res = true;
-//					}
-				//Solution 2:
-				if (helper[indexHelper] == 0) {
-					helper[sum - indexHelper] = 1;
-				} else {
+			short value = array[index];
+			short secondValue = (short) (sum - value);
+			if (secondValue >= 0) {
+				if (helper[secondValue]) {
 					res = true;
+				} else {
+					helper[value] = true;
 				}
-				
 			}
 			index++;
 		}
@@ -46,23 +40,29 @@ public class InitialAlgorithm {
 	}
 
 	public static short getMaxPositiveWithNegativeReflect(short[] array) {
+		// returns maximal positive number, for which there is the negative image
+		// If there are not such numbers at all the method returns -1
 		short res = -1;
-		int[] helper = new int[Short.MAX_VALUE];
-
+		byte[] helper = new byte[Short.MAX_VALUE];
+		short candidate = -1;
 		for (int i = 0; i < array.length; i++) {
+			candidate = (short) Math.abs(array[i]);
+			if (array[i] < 0) {
 
-			int indexHelper = Math.abs(array[i]);
-
-			if (helper[indexHelper] == 0) {
-				helper[indexHelper] = array[i] > 0 ? 1 : -1;
-
-			} else if (helper[indexHelper] > 0) {
-				res = (array[i] < 0 && res < indexHelper) ? (short) indexHelper : res;
-
+				res = getRes(res, helper, candidate, 1);
 			} else {
-				res = (array[i] > 0 && res < indexHelper) ? (short) indexHelper : res;
-
+				res = getRes(res, helper, candidate, -1);
 			}
+		}
+
+		return res;
+	}
+
+	private static short getRes(short res, byte[] helper, short candidate, int sign) {
+		if (helper[candidate] == 1 * sign && candidate > res) {
+			res = candidate;
+		} else if (helper[candidate] == 0) {
+			helper[candidate] = (byte) (-1 * sign);
 		}
 		return res;
 	}
@@ -82,6 +82,33 @@ public class InitialAlgorithm {
 				}
 			}
 		} while (flUnSort);
+	}
+
+	public static <T> int binarySearch(T[] array, T key, Comparator<T> comp) {
+		int leftIndex = 0;
+		int rightIndex = array.length - 1;
+		int middleIndex = rightIndex / 2;
+		int compRes = 0;
+		boolean flFound = false;
+
+		while (!flFound && leftIndex <= rightIndex) {
+			compRes = comp.compare(key, array[middleIndex]);
+			if (compRes > 0) {
+				leftIndex = middleIndex + 1;
+			} else if (compRes < 0) {
+				rightIndex = middleIndex - 1;
+			} else {
+				if (middleIndex!=0 && (comp.compare(array[middleIndex], array[middleIndex - 1])) == 0) {
+					rightIndex = middleIndex - 1;
+				} else {
+					flFound = true;
+				}
+			}
+			middleIndex = (leftIndex + rightIndex) / 2;
+		}
+
+		return leftIndex > rightIndex ? -leftIndex-1 : middleIndex;
+
 	}
 
 }
--- src/telran/algorithm/test/InitialAlgorithmTest.java
+++ src/telran/algorithm/test/InitialAlgorithmTest.java
@@ -2,6 +2,8 @@ package telran.algorithm.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Comparator;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -45,6 +47,9 @@ class InitialAlgorithmTest {
 
 		short[] numbersMiddleSum = { 8, 1, 2, 3, 3 };
 		assertTrue(InitialAlgorithm.isSum2(numbersMiddleSum, (short) 6));
+
+		short[] array1 = { 30000, 1, 5, 2, 10000, 0, 500, 0, Short.MAX_VALUE };
+		assertTrue(InitialAlgorithm.isSum2(array1, Short.MIN_VALUE));
 	}
 
 	@Test
@@ -61,6 +66,25 @@ class InitialAlgorithmTest {
 		assertTrue(runSortedAscendingArrayTest(randomArray));
 	}
 
+	@Test
+	void testBinarySearch() {
+		Integer[] numbersNo0 = { 1, 1, 2, 4, 5 };
+		Integer[] numbersNo6 = { 0, 1, 2, 3, 4, 5 };
+		Integer[] numbersNo2 = {0, 1, 3, 4, 5}; 
+		assertEquals(-1, InitialAlgorithm.binarySearch(numbersNo0, 0, Integer::compareTo));
+		assertEquals(-7, InitialAlgorithm.binarySearch(numbersNo6, 6, Integer::compareTo));
+		assertEquals(-3, InitialAlgorithm.binarySearch(numbersNo2, 2, Integer::compareTo));
+		
+		Integer[] numbersSearch2 = {1,2,2,2,2,3,4};
+		Integer[] numbersSearch1 = {1,1,1,1,1,1,1};
+		Integer[] numbersSearch7 = {0,1,2,3,4,6,7,7};
+		assertEquals(1, InitialAlgorithm.binarySearch(numbersSearch2, 2,Integer::compareTo ));
+		assertEquals(0, InitialAlgorithm.binarySearch(numbersSearch1, 1,Integer::compareTo ));
+		assertEquals(6, InitialAlgorithm.binarySearch(numbersSearch7, 7,Integer::compareTo ));		
+		
+		
+	}
+
 	private boolean runSortedAscendingArrayTest(short[] array) {
 		int counter = 0;
 		for (int i = 0; i < array.length - 1; i++) {
@@ -71,6 +95,32 @@ class InitialAlgorithmTest {
 
 		return counter == 0;
 
+	}
+
+	@Test
+	void maxValueComplexityNTest() {
+		assertEquals(Integer.MAX_VALUE, getMaxValueComplexityN());
+	}
+
+	@Test
+	void maxValueComplexityLogNTest() {
+		assertEquals(Integer.MAX_VALUE, getMaxValueComplexityLogN());
+	}
+
+	private Integer getMaxValueComplexityN() {
+		int res = 1;
+		while (res > 0) {
+			res++;
+		}
+		return res - 1;
+	}
+
+	private Integer getMaxValueComplexityLogN() {
+		int res = 1;
+		while (res > 0) {
+			res *= 2;
+		}
+		return res - 1;
 	}
 
 	private short[] getRandomArray(int size) {
