5edc69a37e928a962680eff7e7dd3d5d46853be7
LudaTverday <Ludachka22@gmail.com>
2023-05-07 20:50:16 +0300

    memory allocation map

--- src/telran/memoryServiceTest/MemoryServiceTest.java
+++ src/telran/memoryServiceTest/MemoryServiceTest.java
@@ -2,6 +2,8 @@ package telran.memoryServiceTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import telran.algorithm.MemoryService;
@@ -23,6 +25,38 @@ class MemoryServiceTest {
 		}
 		
 		assertTrue(flException);
+	}
+	
+	@Test
+	void allocationMemoryTestWithGC() {
+		Runtime runtime = Runtime.getRuntime();
+		for(int i = 0; i<10; i++) {
+			displayMemoryMap(runtime);
+		 long freeMemory = runtime.freeMemory();
+		 int size = freeMemory > Integer.MAX_VALUE ? Integer.MAX_VALUE :(int) freeMemory;
+		 byte[] array = new byte[size];
+		}
+	}
+	
+	@Test
+	void allocationMemoryTestNoGC () {
+		List<byte[]> list = new ArrayList<>();
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			while(true) {
+				displayMemoryMap(runtime);
+				 long freeMemory = runtime.freeMemory();
+				 int size = freeMemory > Integer.MAX_VALUE ? Integer.MAX_VALUE :(int) freeMemory;
+				list.add(new byte[size]);
+			}
+		} catch (OutOfMemoryError e) {
+			
+		}
+	}
+
+	private void displayMemoryMap(Runtime runtime) {
+		System.out.printf("free memory: %d, total memory: %d, maximal memory: %d\n", runtime.freeMemory(), runtime.totalMemory(), runtime.maxMemory());
+		
 	}
 
 }
