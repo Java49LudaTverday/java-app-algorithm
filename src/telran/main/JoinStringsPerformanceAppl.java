cfb5695ef82b59394d4f4b95db4b1beba338021c
LudaTverday <Ludachka22@gmail.com>
2023-10-26 11:23:41 +0300

    solution for using interface of JoinStrings by class_reflection scenario

--- src/telran/main/JoinStringsPerformanceAppl.java
+++ src/telran/main/JoinStringsPerformanceAppl.java
@@ -1,25 +1,39 @@
 package telran.main;
 
 import telran.strings.*;
+
+import java.lang.reflect.Constructor;
+
 import telran.performance.*;
 
 public class JoinStringsPerformanceAppl {
 
 	private static final int N_STRINGS = 1000;
 	private static final int N_RUNS = 10000;
+	private static final String BASE_NAME = "telran.strings.";
 //FIXME rewrite the code by applying class reflection
 	// to get rid of  JoinString implementation
-	public static void main(String[] args) {
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args)  {
 		String[] strings = getBigArrayStrings(N_STRINGS);
-		JoinStrings joinStringsBuilder = new JoinStringsBuilderImpl();
-		JoinStrings joinStrings = new JoinStringsImpl();
-
-		PerformanceTest testJoinStrings = new JoinStringsPerformanceTest("JoinStrings", N_RUNS, strings, joinStrings);
-		PerformanceTest testJoinStringsBuilder = new JoinStringsPerformanceTest("JoinStringsBuilder", N_RUNS, strings,
-				joinStringsBuilder);
-
-		testJoinStrings.run();
-		testJoinStringsBuilder.run();
+		if(args.length < 1) {
+			System.out.println("Too low number of args");
+		} else {
+			for(String arg: args) {
+			try {
+				Class<JoinStrings> clazz = (Class<JoinStrings>) Class.forName(BASE_NAME + arg);
+				Constructor<JoinStrings> constructor = clazz.getConstructor();
+				JoinStrings joinString = constructor.newInstance();
+				PerformanceTest testJoinStrings = new JoinStringsPerformanceTest(arg, N_RUNS, strings, joinString );
+				testJoinStrings.run();
+				
+			} catch (ClassNotFoundException e) {
+				System.out.println(e.getMessage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}			
+			}
+		}
 
 	}
 
